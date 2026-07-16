b59725a8d7da07080831471be93e87bf81cf61ce
annguyen2k8 <hacker.an1901@gmail.com>
2026-07-16 11:13:51 +0700

    Monitor chat updates and message handling adjustments

--- cogs/monitorchat/main.py
+++ cogs/monitorchat/main.py
@@ -42,17 +42,21 @@ class MonitorChat(commands.Cog):
         if not message.content:
             return
         
+        content = escape(message.content, mass_mentions=True)
         author = message.author
         avatar_url = author.avatar.url if author.avatar else None
-        
         username = author.name
+        
+        files = []
+        
         if isinstance(author, discord.Member):
             username = author.display_name
         
-        await self.webhook.send(message.content, username=username, avatar_url=avatar_url)
+        for attachment in message.attachments:
+            files.append(attachment.to_file())
+        
+        await self.webhook.send(content, username=username, avatar_url=avatar_url, files=files)
 
-            
-            
 
 async def setup(bot: BotBase) -> None:
     await bot.add_cog(MonitorChat(bot))
